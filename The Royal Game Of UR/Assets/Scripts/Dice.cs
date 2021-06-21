54d4d567052e16d40387f591d3aaf6ae9a843189
fluxcabury <mark.williamson02@outlook.com>
2021-06-21 14:51:58 +0100

    Uploading the unity files

---
+++ The Royal Game Of UR/Assets/Scripts/Dice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Dice : MonoBehaviour
+{
+    public Text Dice_UIText;
+    private int Dice1;
+    private int Dice2;
+    private int Dice3;
+    private int Dice4;
+    private string Output;
+
+    void Start()
+    {
+
+    }
+
+    void OnMouseUp() {
+        Dice1 = UnityEngine.Random.Range(0,2);
+        Dice2 = UnityEngine.Random.Range(0,2);
+        Dice3 = UnityEngine.Random.Range(0,2);
+        Dice4 = UnityEngine.Random.Range(0,2);
+        int Sum = Dice1 + Dice2 + Dice3 + Dice4;
+        Output = Sum.ToString();
+        Dice_UIText.text = Output;
+        
+    }
+}
---
+++ The Royal Game Of UR/Assets/Scripts/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    private float startPosx;
+    private float startPosy;
+    private bool isBeingHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isBeingHeld == true){
+
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosx, mousePos.y - startPosy, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Vector3 mousePos;
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        startPosx = mousePos.x - this.transform.localPosition.x;
+        startPosy = mousePos.y - this.transform.localPosition.y;
+
+        isBeingHeld = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+}
